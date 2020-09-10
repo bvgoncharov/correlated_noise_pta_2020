9efeb6847621ba7e3bee11839530b0874685a72f
Boris Goncharov <goncharov.boris@physics.msu.ru>
2020-09-10 11:46:11 +1000

    Added slurm submission scripts

---
+++ slurm/ppta_dr2_ptmcmc_ms_gwb_fixslope_set_4_1_ephem_0_20200909.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ppta_ptmcmc_ms_gwb_fixslope_set_4_1_ephem_0
+#SBATCH --output=/fred/oz002/bgoncharov/correlated_noise_logs/ppta_ptmcmc_ms_gwb_fixslope_set_4_1_ephem_0_%A_%a.out
+#SBATCH --ntasks=4
+#SBATCH --time=2-0
+#SBATCH --mem-per-cpu=3G
+#SBATCH --tmp=4G
+#SBATCH --array=0
+
+module load numpy/1.16.3-python-2.7.14
+
+srun echo $TEMPO2
+srun echo $TEMPO2_CLOCK_DIR
+srun python /home/bgonchar/correlated_noise_pta_2020/run_analysis.py --prfile "/home/bgonchar/correlated_noise_pta_2020/params/ppta_dr2_ptmcmc_ms_gwb_fixslope_set_4_1_ephem_0_20200909.dat" --num $SLURM_ARRAY_TASK_ID
---
+++ slurm/ppta_dr2_ptmcmc_ms_gwb_fixslope_set_all_ephem_0_20200910.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ppta_ptmcmc_ms_gwb_fixslope_set_all_ephem_0
+#SBATCH --output=/fred/oz002/bgoncharov/correlated_noise_logs/ppta_ptmcmc_ms_gwb_fixslope_set_all_ephem_0_%A_%a.out
+#SBATCH --ntasks=4
+#SBATCH --time=3-0
+#SBATCH --mem-per-cpu=4G
+#SBATCH --tmp=4G
+#SBATCH --array=0
+
+module load numpy/1.16.3-python-2.7.14
+
+srun echo $TEMPO2
+srun echo $TEMPO2_CLOCK_DIR
+srun python /home/bgonchar/correlated_noise_pta_2020/run_analysis.py --prfile "/home/bgonchar/correlated_noise_pta_2020/params/ppta_dr2_ptmcmc_ms_gwb_fixslope_set_all_ephem_0_20200910.dat" --num $SLURM_ARRAY_TASK_ID
